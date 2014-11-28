795c9354f38440edd153f7477a4448b1e0c29822
Sebastian Kalinowski <sebastian@kalinowski.eu>
2014-11-28 19:35:48 +0100

    start writing tests

---
+++ appfy/recipe/gae/tests/test_sdk.py
@@ -0,0 +1,11 @@
+from unittest2 import TestCase
+
+
+class BaseTestCase(TestCase):
+    pass
+
+
+class TestSDK(BaseTestCase):
+
+    def test_download_sdk_from_given_url(self):
+        assert 0
--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ def get_readme():
 tests_require = (
     'hacking',
     'nose',
+    'unittest2',
 )
 
 
